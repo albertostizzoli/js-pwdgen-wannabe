5a571b1838ba6b9b28a583577a5ace1d1fb154c7
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-06 14:57:53 +0200

    Added Script

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Età
+const age = 21;
+
+//Nome
+
+let nome = prompt('Dimmi il tuo nome');
+
+
+//Cognome
+
+let cognome = prompt('Dimmi il tuo cognome');
+
+//Colore Preferito
+
+let colore = prompt('Dimmi il tuo colore preferito');
+
+
+
+//Password
+
+ let password = ` ${nome} + ${cognome} + ${colore} + ${age}`;
+ 
+ console.log(nome);
+ console.log(cognome);
+ console.log(colore);
+ console.log(age);
+
+ document.getElementById('text').innerHTML;
+ console.log( document.getElementById('text').innerHTML);
