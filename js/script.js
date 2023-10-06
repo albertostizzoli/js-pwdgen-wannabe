06ed21334738c1d02846ba582c32fc7b2b380958
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-06 15:14:00 +0200

    Modifcations

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const age = 21;
 
 let nome = prompt('Dimmi il tuo nome');
 
-
 //Cognome
 
 let cognome = prompt('Dimmi il tuo cognome');
@@ -14,14 +13,12 @@ let cognome = prompt('Dimmi il tuo cognome');
 
 let colore = prompt('Dimmi il tuo colore preferito');
 
-
-
 //Password
 
- let password = `${nome} + ${cognome} + ${colore} + ${age}`;
+ let password = `${nome}${cognome}${colore}${age}`;
  
  console.log(password);
 
  document.getElementById('text').innerHTML = password;
 
- console.log( document.getElementById('text').innerHTML) = password;
+ console.log(document.getElementById('text').innerHTML);
